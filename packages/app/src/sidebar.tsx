7073d765282679e944dbc5128ea4c0b7774e19bb
VW82437 <luca.menzel1@volkswagen.de>
2021-03-11 11:11:14 +0100

    added test plugin to sidebar

--- packages/app/src/sidebar.tsx
+++ packages/app/src/sidebar.tsx
@@ -66,6 +66,7 @@ export const AppSidebar = () => (
     <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
     <SidebarDivider />
     <SidebarItem icon={MapIcon} to="tech-radar" text="Tech Radar" />
+    <SidebarItem icon={LibraryBooks} to="/testplugin" text="Test plugin" />
     {/* End global nav */}
     <SidebarDivider />
     <SidebarSpace />
